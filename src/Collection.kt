4235b63ed6339e712c86725b3543f43a713fcaf1
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-02-06 21:25:30 +0300

    Map collection in Kotlin

--- src/Collection.kt
+++ src/Collection.kt
@@ -17,16 +17,32 @@ val newSolarSystem = arrayOf(
 )
 
 fun main() {
-    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14)
     println(solarSystem.size)
-    solarSystem.add("Pluto")
-    println(solarSystem.contains("Pluto"))
-    println("Pluto" in solarSystem)
-    solarSystem.add("Pluto")
-    println(solarSystem.size)
+    solarSystem["Pluto"] = 5
+    println(solarSystem["Pluto"])
+    println(solarSystem["Theia"])
     solarSystem.remove("Pluto")
-    println(solarSystem.size)
-    println(solarSystem.contains("Pluto"))
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
+//    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+//    println(solarSystem.size)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.contains("Pluto"))
+//    println("Pluto" in solarSystem)
+//    solarSystem.add("Pluto")
+//    println(solarSystem.size)
+//    solarSystem.remove("Pluto")
+//    println(solarSystem.size)
+//    println(solarSystem.contains("Pluto"))
 //    solarSystem.add("Pluto")
 //    solarSystem.add(3, "Theia")
 //    solarSystem[3] = "Future Moon"
